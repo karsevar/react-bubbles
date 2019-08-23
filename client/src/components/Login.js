5c5e024eaf6afd9787bfa4f74bccc98d2fff1d68
masonkarsevar <masonkarsevar@gmail.com>
2019-08-23 08:16:49 -0700

    changeHandler updating useState variable

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({username: '', password: ''})
+
+  
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChange = e => {
+    console.log(credentials)
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+
   return (
     <div className='login-container'>
       <form>
         <input
           type='text'
           name='username'
+          value={credentials.username}
+          onChange={handleChange}
         />
         <input 
-          type='text'
+          type='password'
           name='password'
+          value={credentials.password}
+          onChange={handleChange}
         />
         <button>Login!</button>
       </form>
